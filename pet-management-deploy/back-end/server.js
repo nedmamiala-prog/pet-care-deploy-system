641bd6c8fa2460dafec9eb0e389f3eb81b393875
nedmamiala-prog <nedmamiala@gmail.com>
2025-11-24 22:00:24 +0800

    chore: allow Vercel frontend origin in CORS (pet-care-deploy-system.vercel.app)

--- pet-management-deploy/back-end/server.js
+++ pet-management-deploy/back-end/server.js
@@ -7,7 +7,11 @@ require('./config/schemaMigrations');
 const app = express();
 app.use(express.json({ limit: '10mb' }));
 app.use(cors({
-  origin: ['https://pet-care-mauve-pi.vercel.app', 'http://localhost:3000'],
+  origin: [
+    'https://pet-care-mauve-pi.vercel.app',
+    'https://pet-care-deploy-system.vercel.app',
+    'http://localhost:3000'
+  ],
   credentials: true
 }));
 app.use(express.static('views'));

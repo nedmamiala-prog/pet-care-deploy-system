19a7f12eff4c57ff056e2571e6ba4f5754a00c07
nedmamiala-prog <nedmamiala@gmail.com>
2025-11-25 00:50:18 +0800

    fix(ui): use Link to /signin to avoid full reload and 404

--- pet-management-deploy/front-end/pet-care/src/components/Login.jsx
+++ pet-management-deploy/front-end/pet-care/src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation, Link } from "react-router-dom";
 import "./Login.css";
 import LightRays from "./LightRays";
 import { loginUser, getGoogleAuthUrl } from "../api/authApi";
@@ -144,7 +144,7 @@ export default function Login() {
           <div className="form-footer">
             <p>
               Don’t have an account?{" "}
-              <a href="/Signin" className="link">Sign up</a>
+              <Link to="/signin" className="link">Sign up</Link>
             </p>
           </div>
         </div>
